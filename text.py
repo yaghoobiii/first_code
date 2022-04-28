54e5de345adbd2141c99f1324a275703184b9e6e
maryam <103657696+yaghoobiii@users.noreply.github.com>
2022-04-28 11:37:12 +0430

    Add files via upload

---
+++ text.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+700*900
+
+
+# In[3]:
+
+
+4*5+6
+
+
+# In[4]:
+
+
+4*6/8
+
+
+# In[5]:
+
+
+5*7/9
+
+
+# In[17]:
+
+
+get_ipython().run_line_magic('autocall', '')
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('autocall', '')
+
+
+# In[16]:
+
+
+get_ipython().run_line_magic('autocall', "{'mode'}")
+
+
+# In[23]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[20]:
+
+
+get_ipython().run_line_magic('cd', '')
+
+
+# In[22]:
+
+
+get_ipython().run_line_magic('dhist', '')
+
+
+# In[24]:
+
+
+get_ipython().run_line_magic('edit', '')
+
+
+# In[25]:
+
+
+get_ipython().run_line_magic('env', '')
+
+
+# In[27]:
+
+
+get_ipython().run_line_magic('env', 'var')
+
+
+# In[31]:
+
+
+get_ipython().run_line_magic('gui', 'qt4')
+
+
+# In[51]:
+
+
+get_ipython().run_line_magic('env', 'va wild')
+
+
+# In[32]:
+
+
+get_ipython().run_line_magic('gui', 'qtfor |qt')
+
+
+# In[35]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[37]:
+
+
+get_ipython().run_line_magic('matplotlib', '')
+
+
+# In[38]:
+
+
+get_ipython().run_line_magic('notebook', '')
+
+
+# In[39]:
+
+
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[40]:
+
+
+get_ipython().run_line_magic('precision', '')
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[43]:
+
+
+get_ipython().run_line_magic('pylap', '')
+
+
+# In[45]:
+
+
+get_ipython().run_line_magic('recall', '')
+
+
+# In[ ]:
+
+
+C:\Users\Lenovo
+
+
+# In[46]:
+
+
+get_ipython().run_line_magic('run', '')
+
+
+# In[54]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[48]:
+
+
+get_ipython().run_line_magic('who', '')
+
+
+# In[52]:
+
+
+get_ipython().run_line_magic('gui', 'wx')
+
+
+# In[ ]:
+
+
+
+
